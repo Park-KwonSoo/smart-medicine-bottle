778b2b295741fc01793a9c28dfdb3d826dc30d32
ParkKwonsoo <redbean096@khu.ac.kr>
2021-05-05 16:33:20 +0900

    feat. main server completed

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const Router = require('koa-router');
 const bodyparser = require('koa-bodyparser');
 
 const Mongoose = require('mongoose');
+const api = require('./src/api');
 
 require('dotenv').config();
 const { SERVER_PORT, MONGO_URL } = process.env;
@@ -10,22 +11,21 @@ const { SERVER_PORT, MONGO_URL } = process.env;
 const app = new Koa();
 const router = new Router();
 
-const api = require('./src/api');
 
 Mongoose.connect(MONGO_URL, {
     useNewUrlParser : true,
     useUnifiedTopology: true,
     useCreateIndex : true
 }).then(() => {
-    console.log('Mongo DB is connected');
+    console.log('Mongo DB is connected : ', MONGO_URL);
 }).catch(e => {
     console.log(e, error);
 })
 
 app.use(bodyparser());
 router.use('/api', api.routes());
-app.use(router.routes()).use(allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(SERVER_PORT, () => {
-    console.log(SERVER_PORT, 'is connected');
+    console.log('PORT : ', SERVER_PORT, 'is connected');
 })
